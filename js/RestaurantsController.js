1cd96d83e8c51b09a29efe91026ab05855ea4f6c
Clem Freeman and Ron Park <ronpark9@gmail.com>
2015-02-23 14:42:16 -0800

    Add separate property search inputs

--- js/RestaurantsController.js
+++ js/RestaurantsController.js
@@ -1,7 +1,11 @@
 restaurantList.controller('RestaurantsCtrl', function RestaurantsCtrl($scope) {
   $scope.restaurants = [{name: "Ron's", foodType: "Fried Chicken", location: "Your Momma Ln", priceRange: "$"},
                         {name: "Barack's", foodType: "Fried Chicken", location: "White House", priceRange: "$$$"},
-                        {name: "Clem's", foodType: "Fried Squid", location: "123 Main St", priceRange: "$$"}];
+                        {name: "Joe's", foodType: "Fried Oysters", location: "Detroit", priceRange: "$"},
+                        {name: "Vince's", foodType: "Cajun", location: "Chiraq", priceRange: "$$$"},
+                        {name: "Tiffany's", foodType: "Raw Vegan", location: "8 Mile", priceRange: "$$"},
+                        {name: "Brenda's", foodType: "Chinese", location: "Stoicville", priceRange: "$"},
+                        {name: "Clem's", foodType: "Fried Squid", location: "123 Main st", priceRange: "$$"}];
   $scope.addRestaurant = function() {
     $scope.restaurants.push({name: $scope.restaurantName, foodType: $scope.restaurantFoodType, location: $scope.restaurantLocation, priceRange: $scope.restaurantPriceRange});
     $scope.restaurantName = null;
