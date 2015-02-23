61a94b3db1938c17e3cb2ccc044a1deafbb477b8
Clem Freeman and Ron Park <clemfree@gmail.com>
2015-02-23 11:50:32 -0800

    Add additional restaurant properties

--- js/RestaurantsController.js
+++ js/RestaurantsController.js
@@ -1,8 +1,11 @@
 restaurantList.controller('RestaurantsCtrl', function RestaurantsCtrl($scope) {
   $scope.restaurants = [];
   $scope.addRestaurant = function() {
-    $scope.restaurants.push({name: $scope.restaurantName});
+    $scope.restaurants.push({name: $scope.restaurantName, foodType: $scope.restaurantFoodType, location: $scope.restaurantLocation, priceRange: $scope.restaurantPriceRange});
     $scope.restaurantName = null;
+    $scope.restaurantFoodType = null;
+    $scope.restaurantLocation = null;
+    $scope.restaurantPriceRange = null;
   };
   $scope.deleteRestaurant = function(restaurant) {
     var index = $scope.restaurants.indexOf(restaurant);
