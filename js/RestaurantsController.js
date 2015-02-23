628d1b203ea823f796c255f355a1c01dc33b2f36
Clem Freeman and Ron Park <clemfree@gmail.com>
2015-02-23 12:13:38 -0800

    Add seed data

--- js/RestaurantsController.js
+++ js/RestaurantsController.js
@@ -1,5 +1,7 @@
 restaurantList.controller('RestaurantsCtrl', function RestaurantsCtrl($scope) {
-  $scope.restaurants = [];
+  $scope.restaurants = [{name: "Ron's", foodType: "Fried Chicken", location: "Your Momma Ln", priceRange: "$"},
+                        {name: "Barack's", foodType: "Fried Chicken", location: "White House", priceRange: "$$$"},
+                        {name: "Clem's", foodType: "Fried Squid", location: "123 Main St", priceRange: "$$"}];
   $scope.addRestaurant = function() {
     $scope.restaurants.push({name: $scope.restaurantName, foodType: $scope.restaurantFoodType, location: $scope.restaurantLocation, priceRange: $scope.restaurantPriceRange});
     $scope.restaurantName = null;
