bd2eded5b964f69d7c61a55b685e028d2fa048fb
Clem Freeman and Ron Park <clemfree@gmail.com>
2015-02-23 11:01:21 -0800

    Initial commit

---
+++ js/RestaurantsController.js
@@ -0,0 +1,15 @@
+restaurants.controller('RestaurantsCtrl', function RestaurantsCtrl($scope) {
+  $scope.restaurants = [];
+});
+
+// studentRoster.controller('StudentsCtrl', function StudentsCtrl($scope) {
+//   $scope.students = [];
+//   $scope.addStudent = function() {
+//     $scope.students.push({ name: $scope.studentName });
+//     $scope.studentName = null;
+//   };
+//   $scope.deleteStudent = function(student) {
+//     var index = $scope.students.indexOf(student);
+//     $scope.students.splice(index, 1);
+//   };
+// });
